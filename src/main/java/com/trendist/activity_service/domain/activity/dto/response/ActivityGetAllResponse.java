06602021c9cc819f7ef4b53cf14cba0e955f3eb8
swp1017 <swp1017@naver.com>
2025-05-07 21:19:53 +0900

    feat:활동글 전체 조회 api 구현

--- src/main/java/com/trendist/activity_service/domain/activity/dto/response/ActivityGetAllResponse.java
+++ src/main/java/com/trendist/activity_service/domain/activity/dto/response/ActivityGetAllResponse.java
@@ -1,4 +1,35 @@
 package com.trendist.activity_service.domain.activity.dto.response;
 
-public class ActivityGetAllResponse {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.trendist.activity_service.domain.activity.domain.Activity;
+import com.trendist.activity_service.domain.activity.domain.ActivityType;
+import com.trendist.activity_service.domain.activity.domain.Keyword;
+
+import lombok.Builder;
+
+@Builder
+public record ActivityGetAllResponse(
+	UUID id,
+	String name,
+	ActivityType activityType,
+	Keyword keyword,
+	String siteUrl,
+	String imageUrls,
+	LocalDateTime startDate,
+	LocalDateTime endDate
+) {
+	public static ActivityGetAllResponse from(Activity activity) {
+		return ActivityGetAllResponse.builder()
+			.id(activity.getId())
+			.name(activity.getName())
+			.activityType(activity.getActivityType())
+			.keyword(activity.getKeyword())
+			.siteUrl(activity.getSiteUrl())
+			.imageUrls(activity.getImageUrl())
+			.startDate(activity.getStartDate())
+			.endDate(activity.getEndDate())
+			.build();
+	}
 }
