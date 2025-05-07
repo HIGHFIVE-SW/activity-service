cfa1432440ea75e72a757767008b0e6b6113c3ab
swp1017 <swp1017@naver.com>
2025-05-07 21:26:49 +0900

    feat:Activity 서비스 기능 구현

--- src/main/java/com/trendist/activity_service/domain/activity/service/ActivityService.java
+++ src/main/java/com/trendist/activity_service/domain/activity/service/ActivityService.java
@@ -1,4 +1,56 @@
 package com.trendist.activity_service.domain.activity.service;
 
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.trendist.activity_service.domain.activity.domain.Activity;
+import com.trendist.activity_service.domain.activity.domain.ActivityType;
+import com.trendist.activity_service.domain.activity.domain.Keyword;
+import com.trendist.activity_service.domain.activity.dto.response.ActivityGetAllResponse;
+import com.trendist.activity_service.domain.activity.dto.response.ActivityGetByKeywordResponse;
+import com.trendist.activity_service.domain.activity.dto.response.ActivityGetByTypeResponse;
+import com.trendist.activity_service.domain.activity.repository.ActivityRepository;
+import com.trendist.activity_service.global.exception.ApiException;
+import com.trendist.activity_service.global.response.status.ErrorStatus;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class ActivityService {
+	private final ActivityRepository activityRepository;
+
+	// 모든 활동글 조회
+	public Page<ActivityGetAllResponse> getAllActivities(int page) {
+		Pageable pageable = PageRequest.of(page, 10, Sort.by("createdAt").descending());
+		return activityRepository.findAll(pageable)
+			.map(ActivityGetAllResponse::from);
+	}
+
+	// 리다이렉트를 위한 활동글 조회
+	public Activity getActivityForRedirect(UUID activityId) {
+		return activityRepository.findById(activityId)
+			.orElseThrow(() -> new ApiException(ErrorStatus._ACTIVITY_NOT_FOUND));
+	}
+
+	// 특정 타입의 활동글 조회
+	public Page<ActivityGetByTypeResponse> getActivitiesByType(ActivityType activityType, int page) {
+		Pageable pageable = PageRequest.of(page, 10, Sort.by("createdAt").descending());
+		return activityRepository.findByActivityType(activityType, pageable)
+			.map(ActivityGetByTypeResponse::from);
+	}
+
+	// 특정 키워드의 활동글 조회
+	public Page<ActivityGetByKeywordResponse> getActivitiesByKeyword(Keyword keyword, int page) {
+		Pageable pageable = PageRequest.of(page, 10, Sort.by("createdAt").descending());
+		return activityRepository.findByKeyword(keyword, pageable)
+			.map(ActivityGetByKeywordResponse::from);
+	}
 }
