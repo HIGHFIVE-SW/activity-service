3e36fd8ef98523d4698265986e0088f34cecb56d
swp1017 <swp1017@naver.com>
2025-05-07 21:19:20 +0900

    feat: 활동 타입 enum 설정

--- src/main/java/com/trendist/activity_service/domain/activity/domain/Activity.java
+++ src/main/java/com/trendist/activity_service/domain/activity/domain/Activity.java
@@ -1,4 +1,58 @@
 package com.trendist.activity_service.domain.activity.domain;
 
-public class Activity {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.trendist.activity_service.global.common.domain.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "activity")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Activity extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "activity_id")
+	private UUID id;
+
+	@Column(name = "activity_name")
+	private String name;
+
+	@Column(name = "site_url")
+	private String siteUrl;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "activity_type")
+	private ActivityType activityType;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "keyword")
+	private Keyword keyword;
+
+	@Column(name = "awards")
+	private Boolean awards;
+
+	@Column(name = "start_date")
+	private LocalDateTime startDate;
+
+	@Column(name = "end_date")
+	private LocalDateTime endDate;
+
+	@Column(name = "activity_image_url")
+	private String imageUrl;
 }
