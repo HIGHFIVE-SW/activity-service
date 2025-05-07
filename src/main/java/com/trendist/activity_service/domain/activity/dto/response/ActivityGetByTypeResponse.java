8bc9d3c8bff257e139bd32bd13167be562d3cc99
swp1017 <swp1017@naver.com>
2025-05-07 21:20:33 +0900

    feat:특정 활동 타입 활동글 조회

--- src/main/java/com/trendist/activity_service/domain/activity/dto/response/ActivityGetByTypeResponse.java
+++ src/main/java/com/trendist/activity_service/domain/activity/dto/response/ActivityGetByTypeResponse.java
@@ -1,4 +1,35 @@
 package com.trendist.activity_service.domain.activity.dto.response;
 
-public class ActivityGetByTypeResponse {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.trendist.activity_service.domain.activity.domain.Activity;
+import com.trendist.activity_service.domain.activity.domain.ActivityType;
+import com.trendist.activity_service.domain.activity.domain.Keyword;
+
+import lombok.Builder;
+
+@Builder
+public record ActivityGetByTypeResponse(
+	UUID id,
+	String name,
+	ActivityType activityType,
+	Keyword keyword,
+	String siteUrl,
+	String imageUrl,
+	LocalDateTime startDate,
+	LocalDateTime endDate
+) {
+	public static ActivityGetByTypeResponse from(Activity activity) {
+		return ActivityGetByTypeResponse.builder()
+			.id(activity.getId())
+			.name(activity.getName())
+			.activityType(activity.getActivityType())
+			.keyword(activity.getKeyword())
+			.siteUrl(activity.getSiteUrl())
+			.imageUrl(activity.getImageUrl())
+			.startDate(activity.getStartDate())
+			.endDate(activity.getEndDate())
+			.build();
+	}
 }
