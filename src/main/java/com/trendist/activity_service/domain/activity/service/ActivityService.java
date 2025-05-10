10983853a8c57f729fa3a6859d369f147e7886fd
swp1017 <swp1017@naver.com>
2025-05-10 21:42:00 +0900

    refactor: 불필요한 import문 및 api 삭제

--- src/main/java/com/trendist/activity_service/domain/activity/service/ActivityService.java
+++ src/main/java/com/trendist/activity_service/domain/activity/service/ActivityService.java
@@ -1,7 +1,5 @@
 package com.trendist.activity_service.domain.activity.service;
 
-import java.util.UUID;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,15 +7,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.trendist.activity_service.domain.activity.domain.Activity;
 import com.trendist.activity_service.domain.activity.domain.ActivityType;
 import com.trendist.activity_service.domain.activity.domain.Keyword;
 import com.trendist.activity_service.domain.activity.dto.response.ActivityGetAllResponse;
 import com.trendist.activity_service.domain.activity.dto.response.ActivityGetByKeywordResponse;
 import com.trendist.activity_service.domain.activity.dto.response.ActivityGetByTypeResponse;
 import com.trendist.activity_service.domain.activity.repository.ActivityRepository;
-import com.trendist.activity_service.global.exception.ApiException;
-import com.trendist.activity_service.global.response.status.ErrorStatus;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,12 +27,6 @@ public class ActivityService {
 		Pageable pageable = PageRequest.of(page, 10, Sort.by("createdAt").descending());
 		return activityRepository.findAll(pageable)
 			.map(ActivityGetAllResponse::from);
-	}
-
-	// 리다이렉트를 위한 활동글 조회
-	public Activity getActivityForRedirect(UUID activityId) {
-		return activityRepository.findById(activityId)
-			.orElseThrow(() -> new ApiException(ErrorStatus._ACTIVITY_NOT_FOUND));
 	}
 
 	// 특정 타입의 활동글 조회
