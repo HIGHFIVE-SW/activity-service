81b53b13388f652e617ab0fc505d7efa5a2f9c50
swp1017 <swp1017@naver.com>
2025-05-10 15:03:35 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/com/trendist/activity_service/domain/activity/controller/ActivityController.java
+++ src/main/java/com/trendist/activity_service/domain/activity/controller/ActivityController.java
@@ -1,19 +1,12 @@
 package com.trendist.activity_service.domain.activity.controller;
 
-import java.net.URI;
-import java.util.UUID;
-
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.trendist.activity_service.domain.activity.domain.Activity;
 import com.trendist.activity_service.domain.activity.domain.ActivityType;
 import com.trendist.activity_service.domain.activity.domain.Keyword;
 import com.trendist.activity_service.domain.activity.dto.response.ActivityGetAllResponse;
