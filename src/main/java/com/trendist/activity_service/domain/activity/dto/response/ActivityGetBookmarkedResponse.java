c45ee253f9ede28f342837c4d159562d08693905
swp1017 <swp1017@naver.com>
2025-05-10 21:37:05 +0900

    feat: 북마크한 활동글 조회 api 구현

--- src/main/java/com/trendist/activity_service/domain/activity/dto/response/ActivityGetBookmarkedResponse.java
+++ src/main/java/com/trendist/activity_service/domain/activity/dto/response/ActivityGetBookmarkedResponse.java
@@ -1,4 +1,28 @@
 package com.trendist.activity_service.domain.activity.dto.response;
 
-public class ActivityGetBookmarkedResponse {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.trendist.activity_service.domain.activity.domain.Activity;
+import com.trendist.activity_service.domain.activity.domain.Keyword;
+
+import lombok.Builder;
+
+@Builder
+public record ActivityGetBookmarkedResponse(
+	UUID id,
+	String name,
+	Keyword keyword,
+	LocalDateTime startDate,
+	LocalDateTime endDate
+) {
+	public static ActivityGetBookmarkedResponse from(Activity activity) {
+		return ActivityGetBookmarkedResponse.builder()
+			.id(activity.getId())
+			.name(activity.getName())
+			.keyword(activity.getKeyword())
+			.startDate(activity.getStartDate())
+			.endDate(activity.getEndDate())
+			.build();
+	}
 }
