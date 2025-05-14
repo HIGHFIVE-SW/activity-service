0d3ddd9440f7e0e7569e49504890bc2f1049dc05
swp1017 <swp1017@naver.com>
2025-05-14 13:30:42 +0900

    feat: 활동 북마크 repository 작성

--- src/main/java/com/trendist/activity_service/domain/activity/repository/ActivityBookmarkRepository.java
+++ src/main/java/com/trendist/activity_service/domain/activity/repository/ActivityBookmarkRepository.java
@@ -1,5 +1,6 @@
 package com.trendist.activity_service.domain.activity.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -7,13 +8,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.trendist.activity_service.domain.activity.domain.Activity;
 import com.trendist.activity_service.domain.activity.domain.ActivityBookmark;
 
 public interface ActivityBookmarkRepository extends JpaRepository<ActivityBookmark, UUID> {
-	boolean existsByUserIdAndActivity(UUID userId, Activity activity);
+	Optional<ActivityBookmark> findByUserIdAndActivity_Id(UUID userId, UUID activityId);
 
-	Optional<ActivityBookmark> findByUserIdAndActivity(UUID userId, Activity activity);
+	Boolean existsByUserIdAndActivity_Id(UUID userId, UUID activityId);
+
+	void deleteByUserIdAndActivity_Id(UUID userId, UUID activityId);
 
 	Page<ActivityBookmark> findAllByUserId(UUID userId, Pageable pageable);
+
+	List<ActivityBookmark> findAllByUserIdAndActivity_IdIn(UUID userId, List<UUID> activityIds);
+
 }
