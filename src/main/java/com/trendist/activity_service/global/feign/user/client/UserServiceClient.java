b6fd44974f0eca00636139cdb0fea42c066ee917
swp1017 <swp1017@naver.com>
2025-05-14 13:18:22 +0900

    feat: user-service와의 통신 구현

--- src/main/java/com/trendist/activity_service/global/feign/user/client/UserServiceClient.java
+++ src/main/java/com/trendist/activity_service/global/feign/user/client/UserServiceClient.java
@@ -1,4 +1,26 @@
 package com.trendist.activity_service.global.feign.user.client;
 
-public class UserServiceClient {
+import java.util.UUID;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.trendist.activity_service.global.feign.user.dto.UserProfileResponse;
+import com.trendist.activity_service.global.response.ApiResponse;
+
+@FeignClient(name = "user-service")
+public interface UserServiceClient {
+
+	@GetMapping("/users/profile")
+	ApiResponse<UserProfileResponse> getMyProfile(
+		@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization
+	);
+
+	@GetMapping("/users/profile/{userId}")
+	ApiResponse<UserProfileResponse> getUserProfile(
+		@PathVariable(name = "userId") UUID userId
+	);
 }
--- src/main/java/com/trendist/activity_service/global/feign/user/dto/UserProfileResponse.java
+++ src/main/java/com/trendist/activity_service/global/feign/user/dto/UserProfileResponse.java
@@ -1,4 +1,19 @@
 package com.trendist.activity_service.global.feign.user.dto;
 
-public class UserProfileResponse {
+import java.util.UUID;
+
+import lombok.Builder;
+
+@Builder
+public record UserProfileResponse(
+	UUID id,
+	String username,
+	String email,
+	String nickname,
+	String keyword,
+	String profileUrl,
+	int exp,
+	String tierName,
+	String tierImageUrl
+) {
 }
